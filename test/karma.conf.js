889fe025f2dc6955e9613ff5fc3ab29a9c41a179
cobbdb <cobbdb@gmail.com>
2013-07-30 23:29:03 -0700

    New karma.conf for new version of karma.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,29 +1,78 @@
-module.exports = function (karma) {
-    karma.configure({
-        basePath: '..',
-        frameworks: ['jasmine'],
-        // list of files / patterns to load in the browser
-        files: [
-            'my_module.js',
-            'http://underscorejs.org/underscore-min.js',
-            'test/*.spec.js'
-        ],
-        // list of files to exclude
-        exclude: [],
-        // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-        logLevel: LOG_INFO,
-        browsers: ['Chrome', 'PhantomJS', 'IE'],
-        captureTimeout: 60000,
-        singleRun: true,
-        preprocessors = {
-            'my_fixture.html': 'html2js'
-        },
-        plugins: [
-            'karma-jasmine',
-            'karma-phantomjs-launcher',
-            'karma-chrome-launcher',
-            'karma-ie-launcher',
-            'karma-html2js-preprocessor'
-        ]
-    });
+// Karma configuration
+// Generated on Tue Jul 30 2013 23:27:04 GMT-0700 (Pacific Daylight Time)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '..',
+
+
+    // frameworks to use
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'colorizer.js',
+      'http://underscorejs.org/underscore-min.js',
+      'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
+      'test/*.spec.js',
+      '*.html'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+      
+    ],
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['PhantomJS'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: true,
+    preprocessors: {'*.html': 'html2js'},
+    plugins: [
+        'karma-jasmine',
+        'karma-phantomjs-launcher',
+        'karma-html2js-preprocessor'
+    ]
+  });
 };
