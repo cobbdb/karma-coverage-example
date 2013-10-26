eab7c84323109540aae99a635614d14adb8af9ed
Dan <cobbdb@gmail.com>
2013-10-26 14:08:51 +1300

    Refactor gruntfile.

--- gruntfile.js
+++ gruntfile.js
@@ -2,37 +2,35 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         karma: {
-            once: {
+            default: {
                 options: {
                     autoWatch: false,
                     singleRun: true
-                }
-            },
-            options: {
-                frameworks: ['jasmine'],
-                reporters: ['progress', 'coverage'],
-                port: 9876,
-                colors: true,
-                browsers: ['PhantomJS'],
-                captureTimeout: 10000,
-                reportSlowerThan: 300,
-                files: [
-                    '**/components/**/*',
-                    '**/vendor/**/*'
-                ],
-                preprocessors: {
-                    '**/components/**/tests/**/*.html': 'html2js',
-                    '**/components/**/source/**/*.js': 'coverage'
-                },
-                plugins: [
-                    'karma-jasmine',
-                    'karma-phantomjs-launcher',
-                    'karma-html2js-preprocessor',
-                    'karma-coverage'
-                ],
-                coverageReporter: {
-                    type: 'text-summary',
-                    dir: 'coverage'
+                    frameworks: ['jasmine'],
+                    reporters: ['progress', 'coverage'],
+                    port: 9876,
+                    colors: true,
+                    browsers: ['PhantomJS'],
+                    captureTimeout: 10000,
+                    reportSlowerThan: 300,
+                    files: [
+                        '**/components/**/*',
+                        '**/vendor/**/*'
+                    ],
+                    preprocessors: {
+                        '**/components/**/tests/**/*.html': 'html2js',
+                        '**/components/**/source/**/*.js': 'coverage'
+                    },
+                    plugins: [
+                        'karma-jasmine',
+                        'karma-phantomjs-launcher',
+                        'karma-html2js-preprocessor',
+                        'karma-coverage'
+                    ],
+                    coverageReporter: {
+                        type: 'text-summary',
+                        dir: 'coverage'
+                    }
                 }
             }
         }
